7e2ae8a4a1b26887e804b6bd8d6eb19a74bccfcc
PRESTONPOWELL22 <preston@windowtorecovery.com>
2018-02-02 17:35:40 -0800

    here goes nothing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,8 +17,8 @@ var wins = 0
 // variable for losses
 var losses = 0
 
-// variable for hidden word
-var seceretWord = ""
+// variable for hidden word 
+var seceretWord = []
 
 // loop that goes through the array
 for(i=0; i<arrLength; i++){
@@ -34,11 +34,12 @@ console.log(wordPicker)
 var correctWord = (words[wordPicker])
 console.log(correctWord)
 
+
 var hiddenWord = correctWord.length
 console.log(hiddenWord)
 
 
-
+// This shows the converts the word to _ _ _ and writes it to the html element
 window.onload = function picksWord(){
 for (i = 0 ; i < hiddenWord ; i++){
   seceretWord += "_ "
@@ -50,36 +51,7 @@ document.getElementById("word").innerHTML=seceretWord
 
 
 
-// }
 
-// // function for a hint
-
-// var hint = function () {
-//   if (correctWord === words[0]) {
-//     catagoryName.innerHTML = "Popular CSS Library"
-//   } else if (chosenCategory === categories[1]) {
-//     catagoryName.innerHTML = "Language for styling the web"
-//   } else if (chosenCategory === categories[2]) {
-//     catagoryName.innerHTML = "The basic language of the web"
-//   }
-//   else{
-//     catagoryName.innerHTML = "The programming language of the web"
-//   }
-// }
-
-// comments = function () {
-//   showLives.innerHTML = "You have " + lives + " lives";
-//   if (lives < 1) {
-//     showLives.innerHTML = "Game Over";
-//   }
-//   for (var i = 0; i < geusses.length; i++) {
-//     if (counter + space === geusses.length) {
-//       showLives.innerHTML = "You Win!";
-//     }
-//   }
-// }
-
-//  
 
 
 
